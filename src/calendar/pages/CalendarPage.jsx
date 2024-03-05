8c8d3e81349d5d3f9d39fa4e1e9f36ad94aeaf79
MattoDev <mateofonseca.profesional@gmail.com>
2024-03-05 16:04:32 -0500

    installing calendar dependencies

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,3 +1,48 @@
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+import { format, parse, startOfWeek, getDay, addHours } from "date-fns";
+import enUS from "date-fns/locale/en-US";
+
+import { Navbar } from "../components/Navbar";
+
+const locales = {
+  "en-US": enUS,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+const events = [
+  {
+    title: "Cumpleaños del jefe",
+    notes: "Hay que comprar el pastel",
+    start: new Date(),
+    end: addHours(new Date(), 2),
+    bgColor: "#fafafa",
+    user: {
+      _id: "123",
+      name: "Mateo",
+    },
+  },
+];
+
 export const CalendarPage = () => {
-  return <div>CalendarPage</div>;
+  return (
+    <>
+      <Navbar />
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: "calc( 100vh - 80px)" }}
+      />
+    </>
+  );
 };
