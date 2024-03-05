b34cda47ace17f04421bc3660ec73cf1c023e8b3
MattoDev <mateofonseca.profesional@gmail.com>
2024-03-05 17:49:26 -0500

    Create Calendar Modal

---
+++ src/calendar/components/CalendarModal.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import Modal from "react-modal";
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+Modal.setAppElement("#root");
+export const CalendarModal = () => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  // Make sure to bind modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
+  const onCloseModal = () => {
+    console.log("Cerrando Modal");
+    setIsOpen(false);
+  };
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onCloseModal}
+      style={customStyles}
+      className="modal"
+      overlayClassName="modal-fondo"
+      closeTimeoutMS={200}
+    >
+      <h1>Hola Mundo</h1> <hr />{" "}
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro quis
+        rerum quas repudiandae molestiae, animi dolo
+      </p>
+    </Modal>
+  );
+};
