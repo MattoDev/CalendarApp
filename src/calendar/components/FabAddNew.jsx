ab35f8c964750def96e1ac7a0d8a218e8b97c058
MattoDev <mateofonseca.profesional@gmail.com>
2024-03-11 00:05:33 -0500

    create new event

--- src/calendar/components/FabAddNew.jsx
+++ src/calendar/components/FabAddNew.jsx
@@ -1,3 +1,28 @@
+import { addHours } from "date-fns";
+import { useCalendarStore, useUiStore } from "../../hooks";
+
 export const FabAddNew = () => {
-  return <button className="btn btn-primary fab"></button>;
+  const { OpenDateModal } = useUiStore();
+  const { setActiveEvent } = useCalendarStore();
+
+  const handleClickNew = () => {
+    setActiveEvent({
+      title: "",
+      notes: "",
+      start: new Date(),
+      end: addHours(new Date(), 2),
+      bgColor: "#fafafa",
+      user: {
+        _id: "123",
+        name: "Mateo",
+      },
+    });
+    OpenDateModal();
+  };
+
+  return (
+    <button className="btn btn-primary fab" onClick={handleClickNew}>
+      <i className="fas fa-plus"></i>
+    </button>
+  );
 };
